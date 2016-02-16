eb1816b92cc5479596bdfbe9a158ccc829f805d9
Kost' Koniev <kk@comentality.com>
2016-02-16 03:28:26 +0200

    Transliteration feature

---
+++ DLaB.CrmSvcUtilExtensions/OptionSet/Transliteration/CodeCustomisationService.cs
@@ -0,0 +1,66 @@
+﻿// Define REMOVE_PROXY_TYPE_ASSEMBLY_ATTRIBUTE if you plan on compiling the output from
+// this CrmSvcUtil extension with the output from the unextended CrmSvcUtil in the same
+// assembly (this assembly attribute can only be defined once in the assembly).
+#define REMOVE_PROXY_TYPE_ASSEMBLY_ATTRIBUTE
+
+using System;
+using System.CodeDom;
+using System.Diagnostics;
+using Microsoft.Crm.Services.Utility;
+
+namespace DLaB.CrmSvcUtilExtensions.OptionSet.Transliteration
+{
+    /// <summary>
+    /// Create an implementation of ICustomizeCodeDomService if you want to manipulate the
+    /// code dom after ICodeGenerationService has run.
+    /// </summary>
+    public sealed class CodeCustomizationService : ICustomizeCodeDomService
+    {
+        /// <summary>
+        /// Remove the unnecessary classes that we generated for entities. 
+        /// </summary>
+        public void CustomizeCodeDom(CodeCompileUnit codeUnit, IServiceProvider services)
+        {
+            Trace.TraceInformation("Entering ICustomizeCodeDomService.CustomizeCodeDom");
+            Trace.TraceInformation(
+                "Number of Namespaces generated: {0}", codeUnit.Namespaces.Count);
+
+#if REMOVE_PROXY_TYPE_ASSEMBLY_ATTRIBUTE
+
+            foreach (CodeAttributeDeclaration attribute in codeUnit.AssemblyCustomAttributes)
+            {
+                Trace.TraceInformation("Attribute BaseType is {0}",
+                    attribute.AttributeType.BaseType);
+                if (attribute.AttributeType.BaseType ==
+                    "Microsoft.Xrm.Sdk.Client.ProxyTypesAssemblyAttribute")
+                {
+                    codeUnit.AssemblyCustomAttributes.Remove(attribute);
+                    break;
+                }
+            }
+#endif
+
+            // Iterate over all of the namespaces that were generated.
+            for (var i = 0; i < codeUnit.Namespaces.Count; ++i)
+            {
+                var types = codeUnit.Namespaces[i].Types;
+                Trace.TraceInformation("Number of types in Namespace {0}: {1}",
+                    codeUnit.Namespaces[i].Name, types.Count);
+                // Iterate over all of the types that were created in the namespace.
+                for (var j = 0; j < types.Count; )
+                {
+                    // Remove the type if it is not an enum (all OptionSets are enums) or has no members.
+                    if (!types[j].IsEnum || types[j].Members.Count == 0)
+                    {
+                        types.RemoveAt(j);
+                    }
+                    else
+                    {
+                        j += 1;
+                    }
+                }
+            }
+            Trace.TraceInformation("Exiting ICustomizeCodeDomService.CustomizeCodeDom");
+        }
+    }
+}
---
+++ DLaB.CrmSvcUtilExtensions/OptionSet/Transliteration/FilteringService.cs
@@ -0,0 +1,95 @@
+﻿namespace DLaB.CrmSvcUtilExtensions.OptionSet.Transliteration
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Crm.Services.Utility;
+    using Microsoft.Xrm.Sdk.Metadata;
+
+    /// <summary>
+    /// Create an implementation of ICodeWriterFilterService if you want to provide your own
+    /// logic for whether or not a given item will have code generated for it.
+    /// </summary>
+    public sealed class FilteringService : ICodeWriterFilterService
+    {
+        private Dictionary<string, bool> GeneratedOptionSets { get; set; }
+
+        private ICodeWriterFilterService DefaultService { get; set; }
+
+        public FilteringService(ICodeWriterFilterService defaultService)
+        {
+            DefaultService = defaultService;
+            GeneratedOptionSets = new Dictionary<string, bool>();
+        }
+
+        /// <summary>
+        /// Does not mark the OptionSet for generation if it has already been marked for
+        /// generation.
+        /// </summary>
+        public bool GenerateOptionSet(
+            OptionSetMetadataBase optionSetMetadata, IServiceProvider services)
+        {
+            if (optionSetMetadata.IsGlobal.HasValue && optionSetMetadata.IsGlobal.Value)
+            {
+                if (!GeneratedOptionSets.ContainsKey(optionSetMetadata.Name))
+                {
+                    GeneratedOptionSets[optionSetMetadata.Name] = true;
+                    return true;
+                }
+            }
+            else
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Ideally, we wouldn't generate any attributes, but we must in order to leverage
+        /// the logic in CrmSvcUtil.  If the attribute for an OptionSet is not generated,
+        /// then a null reference exception is thrown when attempting to create the
+        /// OptionSet.  We will remove these in our ICustomizeCodeDomService implementation.
+        /// </summary>
+        public bool GenerateAttribute(
+            AttributeMetadata attributeMetadata, IServiceProvider services)
+        {
+            return (attributeMetadata.AttributeType == AttributeTypeCode.Picklist
+                || attributeMetadata.AttributeType == AttributeTypeCode.State
+                || attributeMetadata.AttributeType == AttributeTypeCode.Status);
+        }
+
+        /// <summary>
+        /// Ideally, we wouldn't generate any entities, but we must in order to leverage
+        /// the logic in CrmSvcUtil.  If an entity which contains a custom OptionSet
+        /// attribute is not generated, then the custom OptionSet will not be generated,
+        /// either.  We will remove these in our ICustomizeCodeDomService implementation.
+        /// </summary>
+        public bool GenerateEntity(
+            EntityMetadata entityMetadata, IServiceProvider services)
+        {
+            return DefaultService.GenerateEntity(entityMetadata, services);
+        }
+
+        /// <summary>
+        /// We don't want to generate any relationships.
+        /// </summary>
+        public bool GenerateRelationship(
+            RelationshipMetadataBase relationshipMetadata, EntityMetadata otherEntityMetadata,
+            IServiceProvider services)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// We don't want to generate any service contexts.
+        /// </summary>
+        public bool GenerateServiceContext(IServiceProvider services)
+        {
+            return false;
+        }
+
+        public bool GenerateOption(OptionMetadata optionMetadata, IServiceProvider services)
+        {
+            return DefaultService.GenerateOption(optionMetadata, services);
+        }
+    }
+}
