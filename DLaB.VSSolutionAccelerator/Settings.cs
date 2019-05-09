4690ec7cae97d1b76ab715f362bd5600fe607c22
Daryl LaBar <d3labar@hotmail.com>
2019-05-09 10:41:37 -0400

    Fix for Unable to add a new plugin/workflow to a solution if the base classes in the shared projects have been renamed #189

--- DLaB.VSSolutionAccelerator/Settings.cs
+++ DLaB.VSSolutionAccelerator/Settings.cs
@@ -1,4 +1,6 @@
-﻿namespace DLaB.VSSolutionAccelerator
+﻿using System;
+
+namespace DLaB.VSSolutionAccelerator
 {
     /// <summary>
     /// This class can help you to store settings for your plugin
@@ -6,6 +8,7 @@
     /// <remarks>
     /// This class must be XML serializable
     /// </remarks>
+    [Serializable]
     public class Settings
     {
         public const string TemplateFolder = "DLaB.VSSolutionAccelerator";
