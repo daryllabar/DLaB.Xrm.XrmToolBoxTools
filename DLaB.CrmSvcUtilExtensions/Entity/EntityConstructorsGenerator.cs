a1bb5d164ba5b5f7cad747892729c0e4d9f5f323
Markus Erlandsson <markus.erlandsson@outlook.com>
2019-10-28 02:25:04 +0100

    draft: create more constructors to use base Entity constructors (#205)
    * draft: create more constructors to use base Entity constructors
    
    * Read and write new setting and place constructors higher up in file, where other constructors are
    
    * Move one position higher

---
+++ DLaB.CrmSvcUtilExtensions/Entity/EntityConstructorsGenerator.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Crm.Services.Utility;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLaB.CrmSvcUtilExtensions.Entity
+{
+    internal class EntityConstructorsGenerator : ICustomizeCodeDomService
+    {
+        public void CustomizeCodeDom(CodeCompileUnit codeUnit, IServiceProvider services)
+        {
+            var types = codeUnit.Namespaces[0].Types;
+
+            foreach (var type in types.Cast<CodeTypeDeclaration>().
+                                 Where(type => type.IsClass && !type.IsContextType()))
+            {
+                AddEntityConstructors(type);
+            }
+        }
+  
+
+        private static void AddEntityConstructors(CodeTypeDeclaration entityClass)
+        {
+            var entityConstructors = typeof(Microsoft.Xrm.Sdk.Entity).GetConstructors();
+            int position = 1;
+
+            foreach (var constructor in entityConstructors)
+            {
+                var codeConstructor = new CodeConstructor
+                {
+                    Attributes = System.CodeDom.MemberAttributes.Public,
+                };
+
+                var paramsToConstructor = constructor.GetParameters();
+
+                // default constructor already there
+                if (paramsToConstructor.Length <= 1)
+                    continue;
+
+                foreach (var param in paramsToConstructor)
+                {
+                    if(param.Name == "entityName")
+                    {
+                        codeConstructor.BaseConstructorArgs.Add(new CodeArgumentReferenceExpression("EntityLogicalName"));
+                    }
+                    else
+                    {
+                        codeConstructor.Parameters.Add(new CodeParameterDeclarationExpression(param.ParameterType, param.Name));
+                        codeConstructor.BaseConstructorArgs.Add(new CodeArgumentReferenceExpression(param.Name));
+                    }
+                }
+
+                entityClass.Members.Insert(position, codeConstructor);
+                position++;
+            }
+        }
+
+    }
+}
