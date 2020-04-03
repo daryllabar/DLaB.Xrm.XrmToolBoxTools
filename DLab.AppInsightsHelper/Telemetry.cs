f2ac8598d630eed568c4b5ae121f7400974d0633
Danish Naglekar <dnaglekar@akaes.com>
2020-04-03 11:21:46 -0400

    Ability to track Settings in AppInsights

---
+++ DLab.AppInsightsHelper/Telemetry.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights;
+
+namespace DLab.AppInsightsHelper
+{
+    public static class Telemetry
+    {
+        private static TelemetryClient _telemetry = GetAppInsightsClient();
+
+        public static bool Enabled { get; set; } = true;
+
+        private static TelemetryClient GetAppInsightsClient()
+        {
+            var config = new TelemetryConfiguration();
+            config.InstrumentationKey = "1e1c8e77-a065-4b97-8e5f-24732732c725";
+            config.TelemetryChannel = new Microsoft.ApplicationInsights.WindowsServer.TelemetryChannel.ServerTelemetryChannel();
+            //config.TelemetryChannel = new Microsoft.ApplicationInsights.Channel.InMemoryChannel(); // Default channel
+
+            TelemetryClient client = new TelemetryClient(config);
+            client.Context.Component.Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            client.Context.Session.Id = Guid.NewGuid().ToString();
+            client.Context.User.Id = (Environment.UserName + Environment.MachineName).GetHashCode().ToString();
+            client.Context.Device.OperatingSystem = Environment.OSVersion.ToString();
+            return client;
+        }
+
+        public static void SetUser(string user)
+        {
+            _telemetry.Context.User.AuthenticatedUserId = user;
+        }
+
+        public static void TrackEvent(string eventName, IDictionary<string, string> properties = null, IDictionary<string, double> metrics = null)
+        {
+            if (Enabled)
+            {
+                _telemetry.TrackEvent(eventName, properties, metrics);
+            }
+        }
+
+        public static void TrackException(Exception ex)
+        {
+            if (ex != null && Enabled)
+            {
+                var telex = new Microsoft.ApplicationInsights.DataContracts.ExceptionTelemetry(ex);
+                _telemetry.TrackException(telex);
+                Flush();
+            }
+        }
+
+        internal static void Flush()
+        {
+            _telemetry.Flush();
+        }
+    }
+}
