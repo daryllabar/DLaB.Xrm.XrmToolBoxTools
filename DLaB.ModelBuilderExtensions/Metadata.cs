a1c0d37f034296d8427ed6e370cfa6f65056a9fb
Daryl LaBar <d3labar@hotmail.com>
2023-11-25 23:47:12 -0500

    Fix for Global OptionSets not generating

--- DLaB.ModelBuilderExtensions/Metadata.cs
+++ DLaB.ModelBuilderExtensions/Metadata.cs
@@ -1,4 +1,7 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
 using DLaB.ModelBuilderExtensions.Serialization;
 using Microsoft.PowerPlatform.Dataverse.ModelBuilderLib;
 using Microsoft.Xrm.Sdk.Metadata;
@@ -6,13 +9,31 @@ using Microsoft.Xrm.Sdk.Metadata;
 namespace DLaB.ModelBuilderExtensions
 {
     [DataContract(Namespace = "http://DLaB.ModelBuilderExtensions")]
-    public class Metadata : IOrganizationMetadata, IExtensibleDataObject
+    public class Metadata : IOrganizationMetadata2, IExtensibleDataObject
     {
+        private List<OptionSetMetadataBase> _optionSetList = new List<OptionSetMetadataBase>();
+        private OptionSetMetadataBase[] _optionSets = Array.Empty<OptionSetMetadataBase>();
+
         [DataMember]
         public EntityMetadata[] Entities { get; set; }
         
         [DataMember]
-        public OptionSetMetadataBase[] OptionSets { get; set; }
+        public OptionSetMetadataBase[] OptionSets {
+            get
+            {
+                if (_optionSets.Length != _optionSetList.Count)
+                {
+                    _optionSets = _optionSetList.ToArray();
+                }
+
+                return _optionSets;
+            }
+            set
+            {
+                _optionSets = value;
+                _optionSetList = _optionSets.ToList();
+            }
+        }
 
         private SdkMessages _messages;
         public SdkMessages Messages
@@ -31,5 +52,12 @@ namespace DLaB.ModelBuilderExtensions
         public MetadataMessages MetadataMessages { get; set; }
 
         public ExtensionDataObject ExtensionData { get; set; }
+
+        public void AddOptionSetInfo(OptionSetMetadata optionSet)
+        {
+            if (_optionSetList.Any(a => a.Name.Equals(optionSet.Name)))
+                return; // skipping. 
+            _optionSetList.Add(optionSet);
+        }
     }
 }
