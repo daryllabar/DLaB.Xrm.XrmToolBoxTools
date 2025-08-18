d013010dc9191c83a2e7c47efe68f3accddbcc2e
Jonas Rapp <jonas@jonasr.app>
2025-08-18 21:18:57 +0200

    Probably almost done with daryllabar/DLaB.Xrm.XrmToolBoxTools#320

--- DLaB.XrmToolBoxCommon/Forms/SpecifyEntitiesDialog.Designer.cs
+++ DLaB.XrmToolBoxCommon/Forms/SpecifyEntitiesDialog.Designer.cs
@@ -34,17 +34,23 @@
             this.BtnRefresh = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.bgEntitiesKeep = new System.Windows.Forms.GroupBox();
-            this.lvKeptEntities = new System.Windows.Forms.ListView();
+            this.lvAvailableEntities = new System.Windows.Forms.ListView();
             this.chDisplayName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.chLogicalName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panFilterAvailable = new System.Windows.Forms.Panel();
+            this.rbAvailPublisher = new System.Windows.Forms.RadioButton();
+            this.rbAvailSolution = new System.Windows.Forms.RadioButton();
+            this.rbAvailAll = new System.Windows.Forms.RadioButton();
+            this.cmbFilterBy = new System.Windows.Forms.ComboBox();
             this.gbEntitiesExclude = new System.Windows.Forms.GroupBox();
-            this.lvExcludedEntities = new System.Windows.Forms.ListView();
+            this.lvSelectedEntities = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.panel4 = new System.Windows.Forms.Panel();
             this.pnlFooter = new System.Windows.Forms.Panel();
             this.tableLayoutPanel1.SuspendLayout();
             this.bgEntitiesKeep.SuspendLayout();
+            this.panFilterAvailable.SuspendLayout();
             this.gbEntitiesExclude.SuspendLayout();
             this.panel4.SuspendLayout();
             this.pnlFooter.SuspendLayout();
@@ -114,31 +120,32 @@
             // 
             // bgEntitiesKeep
             // 
-            this.bgEntitiesKeep.Controls.Add(this.lvKeptEntities);
+            this.bgEntitiesKeep.Controls.Add(this.lvAvailableEntities);
+            this.bgEntitiesKeep.Controls.Add(this.panFilterAvailable);
             this.bgEntitiesKeep.Dock = System.Windows.Forms.DockStyle.Fill;
             this.bgEntitiesKeep.Location = new System.Drawing.Point(3, 3);
             this.bgEntitiesKeep.Name = "bgEntitiesKeep";
             this.bgEntitiesKeep.Size = new System.Drawing.Size(323, 364);
             this.bgEntitiesKeep.TabIndex = 0;
             this.bgEntitiesKeep.TabStop = false;
-            this.bgEntitiesKeep.Text = "All Entities";
+            this.bgEntitiesKeep.Text = "Available Entities";
             // 
-            // lvKeptEntities
+            // lvAvailableEntities
             // 
-            this.lvKeptEntities.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lvAvailableEntities.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.chDisplayName,
             this.chLogicalName});
-            this.lvKeptEntities.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lvKeptEntities.FullRowSelect = true;
-            this.lvKeptEntities.Location = new System.Drawing.Point(3, 16);
-            this.lvKeptEntities.Name = "lvKeptEntities";
-            this.lvKeptEntities.Size = new System.Drawing.Size(317, 345);
-            this.lvKeptEntities.Sorting = System.Windows.Forms.SortOrder.Ascending;
-            this.lvKeptEntities.TabIndex = 2;
-            this.lvKeptEntities.UseCompatibleStateImageBehavior = false;
-            this.lvKeptEntities.View = System.Windows.Forms.View.Details;
-            this.lvKeptEntities.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(Helper.SortListView_OnColumnClick);
-            this.lvKeptEntities.DoubleClick += new System.EventHandler(this.BtnAdd_Click);
+            this.lvAvailableEntities.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvAvailableEntities.FullRowSelect = true;
+            this.lvAvailableEntities.HideSelection = false;
+            this.lvAvailableEntities.Location = new System.Drawing.Point(3, 64);
+            this.lvAvailableEntities.Name = "lvAvailableEntities";
+            this.lvAvailableEntities.Size = new System.Drawing.Size(317, 297);
+            this.lvAvailableEntities.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.lvAvailableEntities.TabIndex = 20;
+            this.lvAvailableEntities.UseCompatibleStateImageBehavior = false;
+            this.lvAvailableEntities.View = System.Windows.Forms.View.Details;
+            this.lvAvailableEntities.DoubleClick += new System.EventHandler(this.BtnAdd_Click);
             // 
             // chDisplayName
             // 
@@ -150,9 +157,69 @@
             this.chLogicalName.Text = "Logical Name";
             this.chLogicalName.Width = 150;
             // 
+            // panFilterAvailable
+            // 
+            this.panFilterAvailable.Controls.Add(this.rbAvailPublisher);
+            this.panFilterAvailable.Controls.Add(this.rbAvailSolution);
+            this.panFilterAvailable.Controls.Add(this.rbAvailAll);
+            this.panFilterAvailable.Controls.Add(this.cmbFilterBy);
+            this.panFilterAvailable.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panFilterAvailable.Location = new System.Drawing.Point(3, 16);
+            this.panFilterAvailable.Name = "panFilterAvailable";
+            this.panFilterAvailable.Size = new System.Drawing.Size(317, 48);
+            this.panFilterAvailable.TabIndex = 10;
+            // 
+            // rbAvailPublisher
+            // 
+            this.rbAvailPublisher.AutoSize = true;
+            this.rbAvailPublisher.Location = new System.Drawing.Point(48, 3);
+            this.rbAvailPublisher.Name = "rbAvailPublisher";
+            this.rbAvailPublisher.Size = new System.Drawing.Size(68, 17);
+            this.rbAvailPublisher.TabIndex = 2;
+            this.rbAvailPublisher.Text = "Publisher";
+            this.rbAvailPublisher.UseVisualStyleBackColor = true;
+            this.rbAvailPublisher.CheckedChanged += new System.EventHandler(this.PlummingFilterAvailable);
+            // 
+            // rbAvailSolution
+            // 
+            this.rbAvailSolution.AutoSize = true;
+            this.rbAvailSolution.Location = new System.Drawing.Point(122, 3);
+            this.rbAvailSolution.Name = "rbAvailSolution";
+            this.rbAvailSolution.Size = new System.Drawing.Size(63, 17);
+            this.rbAvailSolution.TabIndex = 3;
+            this.rbAvailSolution.Text = "Solution";
+            this.rbAvailSolution.UseVisualStyleBackColor = true;
+            this.rbAvailSolution.CheckedChanged += new System.EventHandler(this.PlummingFilterAvailable);
+            // 
+            // rbAvailAll
+            // 
+            this.rbAvailAll.AutoSize = true;
+            this.rbAvailAll.Checked = true;
+            this.rbAvailAll.Location = new System.Drawing.Point(6, 3);
+            this.rbAvailAll.Name = "rbAvailAll";
+            this.rbAvailAll.Size = new System.Drawing.Size(36, 17);
+            this.rbAvailAll.TabIndex = 1;
+            this.rbAvailAll.TabStop = true;
+            this.rbAvailAll.Text = "All";
+            this.rbAvailAll.UseVisualStyleBackColor = true;
+            this.rbAvailAll.CheckedChanged += new System.EventHandler(this.PlummingFilterAvailable);
+            // 
+            // cmbFilterBy
+            // 
+            this.cmbFilterBy.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbFilterBy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFilterBy.Enabled = false;
+            this.cmbFilterBy.FormattingEnabled = true;
+            this.cmbFilterBy.Location = new System.Drawing.Point(1, 24);
+            this.cmbFilterBy.Name = "cmbFilterBy";
+            this.cmbFilterBy.Size = new System.Drawing.Size(313, 21);
+            this.cmbFilterBy.TabIndex = 3;
+            this.cmbFilterBy.SelectedIndexChanged += new System.EventHandler(this.ShowEntitiesByFiltering);
+            // 
             // gbEntitiesExclude
             // 
-            this.gbEntitiesExclude.Controls.Add(this.lvExcludedEntities);
+            this.gbEntitiesExclude.Controls.Add(this.lvSelectedEntities);
             this.gbEntitiesExclude.Dock = System.Windows.Forms.DockStyle.Fill;
             this.gbEntitiesExclude.Location = new System.Drawing.Point(372, 3);
             this.gbEntitiesExclude.Name = "gbEntitiesExclude";
@@ -161,22 +228,22 @@
             this.gbEntitiesExclude.TabStop = false;
             this.gbEntitiesExclude.Text = "Selected Entities";
             // 
-            // lvExcludedEntities
+            // lvSelectedEntities
             // 
-            this.lvExcludedEntities.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lvSelectedEntities.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2});
-            this.lvExcludedEntities.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lvExcludedEntities.FullRowSelect = true;
-            this.lvExcludedEntities.Location = new System.Drawing.Point(3, 16);
-            this.lvExcludedEntities.Name = "lvExcludedEntities";
-            this.lvExcludedEntities.Size = new System.Drawing.Size(318, 345);
-            this.lvExcludedEntities.Sorting = System.Windows.Forms.SortOrder.Ascending;
-            this.lvExcludedEntities.TabIndex = 7;
-            this.lvExcludedEntities.UseCompatibleStateImageBehavior = false;
-            this.lvExcludedEntities.View = System.Windows.Forms.View.Details;
-            this.lvExcludedEntities.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(Helper.SortListView_OnColumnClick);
-            this.lvExcludedEntities.DoubleClick += new System.EventHandler(this.BtnRemove_Click);
+            this.lvSelectedEntities.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvSelectedEntities.FullRowSelect = true;
+            this.lvSelectedEntities.HideSelection = false;
+            this.lvSelectedEntities.Location = new System.Drawing.Point(3, 16);
+            this.lvSelectedEntities.Name = "lvSelectedEntities";
+            this.lvSelectedEntities.Size = new System.Drawing.Size(318, 345);
+            this.lvSelectedEntities.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.lvSelectedEntities.TabIndex = 7;
+            this.lvSelectedEntities.UseCompatibleStateImageBehavior = false;
+            this.lvSelectedEntities.View = System.Windows.Forms.View.Details;
+            this.lvSelectedEntities.DoubleClick += new System.EventHandler(this.BtnRemove_Click);
             // 
             // columnHeader1
             // 
@@ -216,13 +283,16 @@
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.pnlFooter);
             this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(541, 425);
             this.Name = "SpecifyEntitiesDialog";
-            this.Text = "Select Entities";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Select Entities";
             this.Load += new System.EventHandler(this.SpecifyEntitiesDialog_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.bgEntitiesKeep.ResumeLayout(false);
+            this.panFilterAvailable.ResumeLayout(false);
+            this.panFilterAvailable.PerformLayout();
             this.gbEntitiesExclude.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             this.pnlFooter.ResumeLayout(false);
@@ -241,11 +311,16 @@
         private System.Windows.Forms.GroupBox gbEntitiesExclude;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Panel pnlFooter;
-        private System.Windows.Forms.ListView lvKeptEntities;
+        private System.Windows.Forms.ListView lvAvailableEntities;
         private System.Windows.Forms.ColumnHeader chDisplayName;
         private System.Windows.Forms.ColumnHeader chLogicalName;
-        private System.Windows.Forms.ListView lvExcludedEntities;
+        private System.Windows.Forms.ListView lvSelectedEntities;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Panel panFilterAvailable;
+        private System.Windows.Forms.RadioButton rbAvailPublisher;
+        private System.Windows.Forms.RadioButton rbAvailSolution;
+        private System.Windows.Forms.RadioButton rbAvailAll;
+        private System.Windows.Forms.ComboBox cmbFilterBy;
     }
 }
--- DLaB.XrmToolBoxCommon/Forms/SpecifyEntitiesDialog.cs
+++ DLaB.XrmToolBoxCommon/Forms/SpecifyEntitiesDialog.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Query;
+using Source.DLaB.Xrm;
 using XrmToolBox.Extensibility;
 
 // ReSharper disable once CheckNamespace
@@ -10,14 +13,13 @@ namespace DLaB.XrmToolBoxCommon.Forms
 {
     public partial class SpecifyEntitiesDialog : DialogBase
     {
+        private List<EntityMetadata> allEntities;
+        private List<Entity> solutions;
+        private List<Entity> publishers;
+
         public HashSet<string> SpecifiedEntities { get; set; }
 
         #region Constructor / Load
-
-        public SpecifyEntitiesDialog()
-        {
-            InitializeComponent();
-        }
 
         public SpecifyEntitiesDialog(PluginControlBase callingControl)
             : base(callingControl)
@@ -37,28 +39,29 @@ namespace DLaB.XrmToolBoxCommon.Forms
         {
             try
             {
-                lvKeptEntities.BeginUpdate();
-                lvExcludedEntities.BeginUpdate();
+                lvAvailableEntities.BeginUpdate();
+                lvSelectedEntities.BeginUpdate();
                 Enable(false);
-                lvKeptEntities.Items.Clear();
-                lvExcludedEntities.Items.Clear();
-                var localEntities = entities.ToList(); // Keep from multiple Enumerations
+                lvAvailableEntities.Items.Clear();
+                lvSelectedEntities.Items.Clear();
+                allEntities = entities.ToList(); // Keep from multiple Enumerations
 
-                lvExcludedEntities.Items.AddRange(localEntities.Where(e => SpecifiedEntities.Contains(e.LogicalName)).Select(e => new ListViewItem(e.DisplayName.UserLocalizedLabel?.Label ?? "N/A") { SubItems = { e.LogicalName } }).ToArray());
-                lvKeptEntities.Items.AddRange(localEntities.Where(e => !SpecifiedEntities.Contains(e.LogicalName)).Select(e => new ListViewItem(e.DisplayName.UserLocalizedLabel?.Label ?? "N/A") { SubItems = { e.LogicalName } }).ToArray());
+                lvSelectedEntities.Items.AddRange(allEntities.Where(e => SpecifiedEntities.Contains(e.LogicalName)).Select(e => new ListViewItem(e.DisplayName.UserLocalizedLabel?.Label ?? "N/A") { SubItems = { e.LogicalName }, Tag = e }).ToArray());
+                ShowEntitiesByFiltering();
             }
             finally
             {
-                lvKeptEntities.EndUpdate();
-                lvExcludedEntities.EndUpdate();
+                lvAvailableEntities.EndUpdate();
+                lvSelectedEntities.EndUpdate();
                 Enable(true);
             }
         }
 
         private void Enable(bool enable)
         {
-            lvKeptEntities.Enabled = enable;
-            lvExcludedEntities.Enabled = enable;
+            panFilterAvailable.Enabled = enable;
+            lvAvailableEntities.Enabled = enable;
+            lvSelectedEntities.Enabled = enable;
             BtnAdd.Enabled = enable;
             BtnRemove.Enabled = enable;
             BtnSave.Enabled = enable;
@@ -67,7 +70,7 @@ namespace DLaB.XrmToolBoxCommon.Forms
 
         private void BtnSave_Click(object sender, EventArgs e)
         {
-            SpecifiedEntities = new HashSet<string>(lvExcludedEntities.Items.Cast<ListViewItem>().Select(i => i.SubItems[1].Text));
+            SpecifiedEntities = new HashSet<string>(lvSelectedEntities.Items.Cast<ListViewItem>().Select(i => i.SubItems[1].Text));
             DialogResult = DialogResult.OK;
             Close();
         }
@@ -85,22 +88,178 @@ namespace DLaB.XrmToolBoxCommon.Forms
 
         private void BtnAdd_Click(object sender, EventArgs e)
         {
-            var values = lvKeptEntities.SelectedItems.Cast<ListViewItem>().ToArray();
+            var values = lvAvailableEntities.SelectedItems.Cast<ListViewItem>().ToArray();
             foreach (var value in values)
             {
-                lvKeptEntities.Items.Remove(value);
+                lvAvailableEntities.Items.Remove(value);
             }
-            lvExcludedEntities.Items.AddRange(values);
+            lvSelectedEntities.Items.AddRange(values);
         }
 
         private void BtnRemove_Click(object sender, EventArgs e)
         {
-            var values = lvExcludedEntities.SelectedItems.Cast<ListViewItem>().ToArray();
+            var values = lvSelectedEntities.SelectedItems.Cast<ListViewItem>().ToArray();
             foreach (var value in values)
             {
-                lvExcludedEntities.Items.Remove(value);
+                lvSelectedEntities.Items.Remove(value);
             }
-            lvKeptEntities.Items.AddRange(values);
+            lvAvailableEntities.Items.AddRange(values);
         }
+
+        private void PlummingFilterAvailable(object sender = null, EventArgs ea = null)
+        {
+            lvAvailableEntities.Items.Clear();
+            if (allEntities == null)
+            {
+                return; // No entities loaded yet
+            }
+            if (rbAvailSolution.Checked)
+            {
+                if (solutions == null)
+                {
+                    LoadSolutions();
+                }
+                cmbFilterBy.Items.Clear();
+                cmbFilterBy.Items.Add("");
+                cmbFilterBy.Items.AddRange(solutions
+                    .Select(s => new EntityProxy(s, $"{s.GetAttributeValue<string>("friendlyname")} ({s.GetAliasedValue<string>("P.friendlyname")})"))
+                    .ToArray());
+                cmbFilterBy.Enabled = true;
+            }
+            else if (rbAvailPublisher.Checked)
+            {
+                if (publishers == null)
+                {
+                    LoadPublishers();
+                }
+                cmbFilterBy.Items.Clear();
+                cmbFilterBy.Items.Add("");
+                cmbFilterBy.Items.AddRange(publishers
+                    .Select(p => new EntityProxy(p, $"{p.GetAttributeValue<string>("friendlyname")} ({p.GetAliasedValue<string>("customizationprefix")})"))
+                    .ToArray());
+                cmbFilterBy.Enabled = true;
+            }
+            else
+            {
+                // Show all entities
+                lvAvailableEntities.Items.AddRange(allEntities
+                    .Select(e => new ListViewItem(e.DisplayName.UserLocalizedLabel?.Label ?? "N/A") { Tag = e, SubItems = { e.LogicalName } })
+                    .ToArray());
+                cmbFilterBy.Enabled = false;
+            }
+            ShowEntitiesByFiltering();
+        }
+
+        private void ShowEntitiesByFiltering(object sender = null, EventArgs ea = null)
+        {
+            lvAvailableEntities.Items.Clear();
+            var selectedentityguids = new List<Guid>();
+            if (rbAvailAll.Checked)
+            {
+                selectedentityguids = allEntities.Select(e => e.MetadataId ?? Guid.Empty).ToList();
+            }
+            else if (cmbFilterBy.SelectedItem is EntityProxy proxy && proxy.Entity is Entity selected)
+            {
+                var query = new QueryExpression("solutioncomponent");
+                query.ColumnSet.AddColumns("objectid", "solutioncomponentid", "rootcomponentbehavior", "rootsolutioncomponentid", "ismetadata", "componenttype");
+                if (selected.LogicalName == "solution")
+                {
+                    query.Criteria.AddCondition("solutionid", ConditionOperator.Equal, selected.Id);
+                }
+                else if (selected.LogicalName == "publisher")
+                {
+                    var query_solution = query.AddLink("solution", "solutionid", "solutionid");
+                    query_solution.LinkCriteria.AddCondition("publisherid", ConditionOperator.Equal, selected.Id);
+                }
+                else
+                {
+                    query.Criteria.AddCondition("solutionid", ConditionOperator.Null);
+                }
+                query.Criteria.AddCondition("componenttype", ConditionOperator.Equal, 1);
+                try
+                {
+                    var result = CallingControl.Service.RetrieveMultiple(query);
+                    selectedentityguids = result.Entities.Select(e => e.GetAttributeValue<Guid>("objectid")).ToList();
+                }
+                catch (Exception ex)
+                {
+                    CallingControl.ShowErrorDialog(ex, "Loading Solutions Entities");
+                }
+            }
+
+            var selectedentities = lvSelectedEntities.Items.Cast<ListViewItem>().Select(i => i.Tag as EntityMetadata).ToList();
+
+            lvAvailableEntities.Items.AddRange(allEntities
+                .Where(e => selectedentityguids.Contains(e.MetadataId ?? Guid.Empty))
+                .Where(e => !selectedentities.Contains(e))
+                .Select(e => new ListViewItem(e.DisplayName.UserLocalizedLabel?.Label ?? "N/A") { Tag = e, SubItems = { e.LogicalName } })
+                .ToArray());
+        }
+
+        private void LoadSolutions()
+        {
+            if (CallingControl.Service == null)
+            {
+                solutions = new List<Entity>();
+                return;
+            }
+            var query = new QueryExpression("solution");
+            query.ColumnSet.AddColumns("friendlyname", "uniquename", "ismanaged", "isvisible", "version");
+            query.Criteria.AddCondition("isvisible", ConditionOperator.Equal, true);
+            query.AddOrder("ismanaged", OrderType.Descending);
+            query.AddOrder("friendlyname", OrderType.Ascending);
+            var publisher = query.AddLink("publisher", "publisherid", "publisherid");
+            publisher.EntityAlias = "P";
+            publisher.Columns.AddColumns("customizationprefix", "uniquename", "friendlyname");
+            try
+            {
+                solutions = CallingControl.Service.RetrieveMultiple(query).Entities.ToList();
+            }
+            catch (Exception ex)
+            {
+                CallingControl.ShowErrorDialog(ex, "Loading Solutions");
+            }
+        }
+
+        private void LoadPublishers()
+        {
+            if (CallingControl.Service == null)
+            {
+                publishers = new List<Entity>();
+                return;
+            }
+            var query = new QueryExpression("publisher");
+            query.ColumnSet.AddColumns("publisherid", "friendlyname", "customizationprefix");
+            var query_solution = new LinkEntity("publisher", "solution", "publisherid", "publisherid", JoinOperator.Any);
+            query.Criteria.AnyAllFilterLinkEntity = query_solution;
+            query_solution.LinkCriteria.AddCondition("isvisible", ConditionOperator.Equal, true);
+            query.AddOrder("friendlyname", OrderType.Ascending);
+            try
+            {
+                publishers = CallingControl.Service.RetrieveMultiple(query).Entities.ToList();
+            }
+            catch (Exception ex)
+            {
+                CallingControl.ShowErrorDialog(ex, "Loading Publishers");
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
+            }
+        }
+    }
+
+    public class EntityProxy
+    {
+        private string displayName;
+        public Entity Entity;
+
+        public EntityProxy(Entity entity, string displayname)
+        {
+            Entity = entity ?? throw new ArgumentNullException(nameof(entity));
+            displayName = displayname;
+        }
+
+        public override string ToString() => displayName ?? Entity?.Id.ToString() ?? "N/A";
     }
 }
