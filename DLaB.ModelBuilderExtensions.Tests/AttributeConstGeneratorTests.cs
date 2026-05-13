b546f9f2ed09b7806ba9a1045f4782c0bbf5e4aa
Daryl LaBar <9728423+daryllabar@users.noreply.github.com>
2026-05-13 09:51:41 -0400

    Update Name and folder while writing metadata to xml file closes #604 Also test for English fix.

---
+++ DLaB.ModelBuilderExtensions.Tests/AttributeConstGeneratorTests.cs
@@ -0,0 +1,49 @@
+﻿using DLaB.ModelBuilderExtensions.Entity;
+using FakeItEasy;
+using Microsoft.PowerPlatform.Dataverse.ModelBuilderLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.CodeDom;
+using System.Linq;
+
+namespace DLaB.ModelBuilderExtensions.Tests
+{
+    [TestClass]
+    public class AttributeConstGeneratorTests
+    {
+        [TestMethod]
+        public void CustomizeCodeDom_ShouldReplaceFieldsCommentTypo_InConstClassSnippet()
+        {
+            var sut = new AttributeConstGenerator(A.Fake<ICustomizeCodeDomService>(), new DLaBModelBuilderSettings
+            {
+                EmitFieldsClasses = true
+            });
+
+            var code = new CodeCompileUnit();
+            var ns = new CodeNamespace("TestNamespace");
+            code.Namespaces.Add(ns);
+            var entity = new CodeTypeDeclaration("Account")
+            {
+                IsClass = true
+            };
+            entity.CustomAttributes.Add(new CodeAttributeDeclaration("Microsoft.Xrm.Sdk.Client.EntityLogicalNameAttribute", new CodeAttributeArgument(new CodePrimitiveExpression("account"))));
+            entity.Members.Add(new CodeSnippetTypeMember(
+                """
+                		/// <summary>
+                		/// Available fields, a the time of codegen, for the account entity
+                		/// </summary>
+                		public partial class Fields
+                		{
+                			public const string Name = "name";
+                		}
+                """
+            ));
+            ns.Types.Add(entity);
+
+            sut.CustomizeCodeDom(code, null!);
+
+            var fieldsClass = entity.Members.OfType<CodeSnippetTypeMember>().First(m => m.Text.Contains("public partial class Fields"));
+            Assert.IsTrue(fieldsClass.Text.Contains("Available fields, at the time of codegen, for the account entity"));
+            Assert.IsFalse(fieldsClass.Text.Contains("Available fields, a the time of codegen, for the account entity"));
+        }
+    }
+}
